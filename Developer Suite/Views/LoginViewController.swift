fc928bd0f828980d1de3c8c28d9d2676bf713f5a
Raghavan Renganathan <renganathan.raghavan@gmail.com>
2018-10-13 16:40:28 -0400

    Updating views and fonts used

--- Developer Suite/Views/LoginViewController.swift
+++ Developer Suite/Views/LoginViewController.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    // MARK: Properties
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+
+    @IBOutlet weak var signinButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    // MARK: Actions
+    @IBAction func doSignIn(_ sender: UIButton) {
+        let username: String = usernameTextField.text ?? ""
+        let password: String = passwordTextField.text ?? ""
+        
+        
+    }
 }
 
